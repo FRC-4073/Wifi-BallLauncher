8bc9eb2e7952ea965d88f71d6fd91758fc3fdf04
The1TrueJoe <Joseph@Joeys-MacBook.local>
2020-03-09 17:25:20 -0400

    Test

---
+++ Wifi-BallLauncher.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def main():
+   return render_template('main.html')
+
+
+@app.route("/<pin>/<action>")
+def action(pin, action):
+   if pin == "pin1" and action == "on":
+       os.system('echo 1=210 /dev/')
+      
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
